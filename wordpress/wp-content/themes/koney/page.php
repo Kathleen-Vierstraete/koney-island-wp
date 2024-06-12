68787647ec9337c1c1b2ef935b7b58c65aead513
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2024-06-12 13:04:05 +0200

    feat: adding page.php + info in the backoffice

---
+++ wordpress/wp-content/themes/koney/page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template for the static pages
+ */
+
+get_header();
+
+if(have_posts()) :
+    the_post();
+  
+?>
+
+<div class="content">
+            <h2 class="segment__title"><?php the_title(); ?></h2>
+
+            <p> <?php the_content(); ?></p>
+    
+        </div>
+
+
+        <?php
+endif;
+get_footer();        
